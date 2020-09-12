a630e631026d011db0a228b740492ac44fc64367
Amal Alkhamees <amalkms5@gmail.com>
2020-09-12 16:50:21 +0300

    upgrade arkade to latest version
    Signed-off-by: Amal Alkhamees <amalkms5@gmail.com>

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -1,9 +1,9 @@
 class Arkade < Formula
   desc "Install Kubernetes CLIs and Apps"
   homepage "http://get-arkade.dev"
-  url "https://github.com/alexellis/arkade/releases/download/0.6.10/arkade-darwin"
-  sha256 "4990adf521bd263e098fdada1ef2143194d20b98fbacc917de21fdaf46d9e667"
-  version "0.6.10"
+  url "https://github.com/alexellis/arkade/releases/download/0.6.12/arkade-darwin"
+  sha256 "0d0a792f1c0c5f2f36dccbb12f0c1aef60c327449be7a65ab6a94c4ff28bda20"
+  version "0.6.12"
   bottle :unneeded
 
   def install
