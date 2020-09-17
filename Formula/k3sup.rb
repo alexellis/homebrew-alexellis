240a8478c363cfea8f66b5fe68ef29d39c960df4
Amal Alkhamees <amalkms5@gmail.com>
2020-09-17 17:31:04 +0300

    add workflow to automate updating the releases
    Signed-off-by: Amal Alkhamees <amalkms5@gmail.com>

--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -1,14 +1,11 @@
 class K3sup < Formula
-  desc "Install k3s over SSH with ('ketchup')"
-  homepage "http://k3sup.dev"
+  desc "bootstrap Kubernetes with k3s over SSH < 1 min 🚀"
   url "https://github.com/alexellis/k3sup/releases/download/0.9.6/k3sup-darwin"
   sha256 "0177e08d4104bf01182fe347492e70274f53dd19b1161acc7e043b649a4e0e7d"
-
   version "0.9.6"
   bottle :unneeded
 
   def install
       bin.install "k3sup-darwin" => "k3sup"
   end
-
 end
