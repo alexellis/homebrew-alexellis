f9651fc3db75ced007f27f1f33db9291fa7fe8eb
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-09-22 10:24:56 +0100

    Update arkade version to 0.6.13
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -1,9 +1,9 @@
 class Arkade < Formula
   desc "Install Kubernetes CLIs and Apps"
   homepage "http://get-arkade.dev"
-  url "https://github.com/alexellis/arkade/releases/download/0.6.12/arkade-darwin"
-  sha256 "0d0a792f1c0c5f2f36dccbb12f0c1aef60c327449be7a65ab6a94c4ff28bda20"
-  version "0.6.12"
+  url "https://github.com/alexellis/arkade/releases/download/0.6.13/arkade-darwin"
+  sha256 "92ddad06507a9899ddc0b499a0bb80f4e2dccc290cb82cdd1918329b42150148"
+  version "0.6.13"
   bottle :unneeded
 
   def install
