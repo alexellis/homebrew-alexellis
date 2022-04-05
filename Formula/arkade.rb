6e9e681601b2d7279806f30aa02ba5f5c23c55c3
alexellis <alexellis@users.noreply.github.com>
2022-04-05 00:05:46 +0000

    syncup with latest version

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -1,8 +1,8 @@
 class Arkade < Formula
     desc "Open Source Kubernetes Marketplace"
-    url "https://github.com/alexellis/arkade/releases/download/0.8.18/arkade-darwin"
-    sha256 "1577b1d10f95241f2f109099931a9a33b9bef2f69da9f00dd17e3f9d1ba2fd7a"
-    version "0.8.18"
+    url "https://github.com/alexellis/arkade/releases/download/0.8.19/arkade-darwin"
+    sha256 "5af9da5c978027c1fdc7d692f880e9b0845f96f3f6b9f6196d0c6c709d171e80"
+    version "0.8.19"
     bottle :unneeded
     
     def install
