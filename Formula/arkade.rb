ab7506357f3851cae3111a3b6324354b984fdc94
alexellis <alexellis@users.noreply.github.com>
2022-04-12 00:06:18 +0000

    syncup with latest version

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -1,5 +1,5 @@
 class Arkade < Formula
-    desc "Open Source Kubernetes Marketplace"
+    desc "Open Source Marketplace For Developer Tools"
     url "https://github.com/alexellis/arkade/releases/download/0.8.22/arkade-darwin"
     sha256 "ea75bae6b93117a9b77cdfe58cd0b8c8b6b5f843a39f47a862a14502cc73a4f8"
     version "0.8.22"
