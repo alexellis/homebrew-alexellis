17c86360bce258351c8f01b1574d46e8deaf5119
alexellis <alexellis@users.noreply.github.com>
2022-07-01 00:07:37 +0000

    syncup with latest version

--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -1,8 +1,8 @@
 class K3sup < Formula
     desc "bootstrap Kubernetes with k3s over SSH < 1 min 🚀"
-    url "https://github.com/alexellis/k3sup/releases/download/0.11.3/k3sup-darwin"
-    sha256 "36fe7acca008f1a8ccde101b917a397308cf5da679e6ce02284102187dad97e7"
-    version "0.11.3"
+    url "https://github.com/alexellis/k3sup/releases/download/0.12.0/k3sup-darwin"
+    sha256 "d57d11d157382e04b7b4e2b33e62403f11b63dc13e6bed210e75c43769dc7403"
+    version "0.12.0"
     bottle :unneeded
     
     def install
