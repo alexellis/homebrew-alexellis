a2456933ec2e694f8d97c5b8d13dfe52f57b187b
alexellis <alexellis@users.noreply.github.com>
2022-09-05 16:10:38 +0000

    syncup with latest version

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -3,7 +3,6 @@ class Arkade < Formula
     url "https://github.com/alexellis/arkade/releases/download/0.8.41/arkade-darwin"
     sha256 "ca6a4e07958f2c33d42768392d012db18c6de79f5ed9678dda8f40ce1de5ab95"
     version "0.8.41"
-    bottle :unneeded
     
     def install
         bin.install "arkade-darwin" => "arkade"
--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -3,7 +3,6 @@ class K3sup < Formula
     url "https://github.com/alexellis/k3sup/releases/download/0.12.3/k3sup-darwin"
     sha256 "1a417cd8ad93403e7f0e1adcc132804800cefee81912429ca241af1ad6f9a5f4"
     version "0.12.3"
-    bottle :unneeded
     
     def install
         bin.install "k3sup-darwin" => "k3sup"
