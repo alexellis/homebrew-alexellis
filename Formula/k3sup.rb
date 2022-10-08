3dc5f29cdec78a14ecd2d3a427877d08596b7c5c
alexellis <alexellis@users.noreply.github.com>
2022-10-08 00:11:26 +0000

    syncup with latest version

--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -1,8 +1,8 @@
 class K3sup < Formula
     desc "bootstrap K3s over SSH in < 60s 🚀"
-    url "https://github.com/alexellis/k3sup/releases/download/0.12.7/k3sup-darwin"
-    sha256 "57ade64e7a8948c5a886a1666f82ac17bc738b26f190634b2a8e0bd49a34290d"
-    version "0.12.7"
+    url "https://github.com/alexellis/k3sup/releases/download/0.12.8/k3sup-darwin"
+    sha256 "b5e2138e2a2740987338561f40770174f78afcebd6077707395c41d3d01cf153"
+    version "0.12.8"
     
     def install
         bin.install "k3sup-darwin" => "k3sup"
