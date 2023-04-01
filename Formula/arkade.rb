c57ceee4421a76b915d90266145c33dd30da8880
alexellis <alexellis@users.noreply.github.com>
2023-04-01 00:06:41 +0000

    syncup with latest version

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -1,8 +1,8 @@
 class Arkade < Formula
     desc "Open Source Marketplace For Developer Tools"
-    url "https://github.com/alexellis/arkade/releases/download/0.9.6/arkade-darwin"
-    sha256 "f4637937e38bf8f7df7d7f22bc9369b72fe8858f5458b61621b5809f94f33184"
-    version "0.9.6"
+    url "https://github.com/alexellis/arkade/releases/download/0.9.7/arkade-darwin"
+    sha256 "b43410787cc357994fb3f695b5ee541c391aec3870eabd101ca218652137b31e"
+    version "0.9.7"
     
     def install
         bin.install "arkade-darwin" => "arkade"
--- Formula/run-job.rb
+++ Formula/run-job.rb
@@ -1,8 +1,8 @@
 class Run-job < Formula
     desc "Run a Kubernetes Job and get the logs when it's done 🏃‍♂️"
-    url "https://github.com/alexellis/run-job/releases/download/0.0.2/run-job-darwin"
-    sha256 "923fdf7f0f8da0c9197b8db560dae7437c1f0fd48d9ce800fa852d4d6aafbaff"
-    version "0.0.2"
+    url "https://github.com/alexellis/run-job/releases/download/0.0.3/run-job-darwin"
+    sha256 "574cfe1b0043747fbd372335af2ef783dd49ed587d42986f98f0e85f8f2d28c1"
+    version "0.0.3"
     
     def install
         bin.install "run-job-darwin" => "run-job"
