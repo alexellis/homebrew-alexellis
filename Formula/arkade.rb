65ee6e79ffecc399ee9da0e78afe358b17877f77
alexellis <alexellis@users.noreply.github.com>
2023-12-20 00:05:31 +0000

    syncup with latest version

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -1,8 +1,8 @@
 class Arkade < Formula
     desc "Open Source Marketplace For Developer Tools"
-    url "https://github.com/alexellis/arkade/releases/download/0.10.18/arkade-darwin"
-    sha256 "ba05134e714331fc140db237b2fa541536fa4af29307c19566bf7ddc36c1c39b"
-    version "0.10.18"
+    url "https://github.com/alexellis/arkade/releases/download/0.10.20/arkade-darwin"
+    sha256 "a27110b7f9fbac815baacce99096cd3ea94e6ea2d8202845b1f055cbd6020b2a"
+    version "0.10.20"
     
     def install
         bin.install "arkade-darwin" => "arkade"
--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -1,8 +1,8 @@
 class K3sup < Formula
     desc "bootstrap K3s over SSH in < 60s 🚀"
-    url "https://github.com/alexellis/k3sup/releases/download/0.13.3/k3sup-darwin"
-    sha256 "c16f67720f66afa253f82176a2ab6a93877c887e8f93d3c0f5abe02e1f83ea10"
-    version "0.13.3"
+    url "https://github.com/alexellis/k3sup/releases/download/0.13.4/k3sup-darwin"
+    sha256 "2f08d77125997d2b46aa00dd189ab69366a33bc23aa6cc972d3371ac83facc62"
+    version "0.13.4"
     
     def install
         bin.install "k3sup-darwin" => "k3sup"
