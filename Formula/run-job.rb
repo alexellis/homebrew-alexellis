8eb89f11c0b5e6708f49a95ffdc727c494ca5b96
alexellis <alexellis@users.noreply.github.com>
2024-02-17 00:06:22 +0000

    syncup with latest version

--- Formula/run-job.rb
+++ Formula/run-job.rb
@@ -1,8 +1,8 @@
 class Run-job < Formula
     desc "Run a Kubernetes Job and get the logs when it's done 🏃‍♂️"
-    url "https://github.com/alexellis/run-job/releases/download/0.1.1/run-job-darwin"
-    sha256 "2128c56c2a52e5965c9095cba9ff519f017ec3204042ca8ffb62af73fa2980dc"
-    version "0.1.1"
+    url "https://github.com/alexellis/run-job/releases/download/0.1.2/run-job-darwin"
+    sha256 "4af8a704da890733280d86798a53d52550caaf86096ff80e038c948463fa0d78"
+    version "0.1.2"
     
     def install
         bin.install "run-job-darwin" => "run-job"
