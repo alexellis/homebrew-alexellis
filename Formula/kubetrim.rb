d13654672b62d62a751e9ca7dbfa3aa0ee6eb1ff
alexellis <alexellis@users.noreply.github.com>
2024-10-15 07:36:59 +0000

    syncup with latest version

--- Formula/kubetrim.rb
+++ Formula/kubetrim.rb
@@ -1,6 +1,6 @@
 class Kubetrim < Formula
     desc "Trim 📏 your KUBECONFIG automatically"
-    url "https://github.com/alexellis/kubetrim/releases/download/v0.0.1/kubetrim-darwin.tgz"
+    url "https://github.com/alexellis/kubetrim/releases/download/v0.0.1/kubetrim-darwin"
     sha256 "1ab95fa62819396c17abc585bd5f415073b6390c3b4d078e0be02bddb61e5fb0"
     version "v0.0.1"
     
