a3457191b1cc4949edf79e4297f9e043885821cf
alexellis <alexellis@users.noreply.github.com>
2025-07-02 00:10:40 +0000

    syncup with latest version

---
+++ Formula/kubetrim.rb
@@ -0,0 +1,10 @@
+class Kubetrim < Formula
+    desc "Trim 📏 your KUBECONFIG automatically"
+    url "https://github.com/alexellis/kubetrim/releases/download/v0.0.1/kubetrim-darwin"
+    sha256 "1ab95fa62819396c17abc585bd5f415073b6390c3b4d078e0be02bddb61e5fb0"
+    version "v0.0.1"
+    
+    def install
+        bin.install "kubetrim-darwin" => "kubetrim"
+    end
+end
