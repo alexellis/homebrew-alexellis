a779d9cb8f2a27b2c13a94114ac9e7401a8c96ee
alexellis <alexellis@users.noreply.github.com>
2025-10-04 00:09:18 +0000

    syncup with latest version

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -1,8 +1,8 @@
 class Arkade < Formula
     desc "Open Source Marketplace For Developer Tools"
-    url "https://github.com/alexellis/arkade/releases/download/0.11.47/arkade-darwin"
-    sha256 "320489ca3ce4bb064b6722cad6329752739c014004e52e5861dd160bb2f29db4"
-    version "0.11.47"
+    url "https://github.com/alexellis/arkade/releases/download/0.11.50/arkade-darwin"
+    sha256 "9f34e4925dd38e54875e2f343c6578e09301439f1c9e4fa878325322f7022af2"
+    version "0.11.50"
     
     def install
         bin.install "arkade-darwin" => "arkade"
