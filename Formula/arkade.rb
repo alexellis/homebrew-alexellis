1c91853a5abf76579e176143d330437b3aa19f50
alexellis <alexellis@users.noreply.github.com>
2026-02-08 00:19:00 +0000

    syncup with latest version

--- Formula/arkade.rb
+++ Formula/arkade.rb
@@ -1,8 +1,8 @@
 class Arkade < Formula
     desc "Open Source Marketplace For Developer Tools"
-    url "https://github.com/alexellis/arkade/releases/download/0.11.70/arkade-darwin"
-    sha256 "4020adc376deb0122622e8efea1a22829c16efdfe24b543404c4fe49d0caa4b3"
-    version "0.11.70"
+    url "https://github.com/alexellis/arkade/releases/download/0.11.72/arkade-darwin"
+    sha256 "7e6c3dd3efd5e07d64b70ba312198b7c7d4664cb702418219efe12b3e882503e"
+    version "0.11.72"
     
     def install
         bin.install "arkade-darwin" => "arkade"
--- Formula/kubetrim.rb
+++ Formula/kubetrim.rb
@@ -1,8 +1,8 @@
 class Kubetrim < Formula
     desc "Trim 📏 your KUBECONFIG automatically"
-    url "https://github.com/alexellis/kubetrim/releases/download/v0.0.1/kubetrim-darwin"
-    sha256 "1ab95fa62819396c17abc585bd5f415073b6390c3b4d078e0be02bddb61e5fb0"
-    version "v0.0.1"
+    url "https://github.com/alexellis/kubetrim/releases/download/v0.0.2/kubetrim-darwin"
+    sha256 "44459e41997298729d43f0adc5b0273cbc375018bcdec21621ce6e09a919e4c0"
+    version "v0.0.2"
     
     def install
         bin.install "kubetrim-darwin" => "kubetrim"
